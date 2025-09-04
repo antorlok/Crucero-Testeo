674140873196f284b679a0e5d6fbe69fd2aa0901
Cristian De Abreu <cdeabreu907@gmail.com>
2025-09-04 00:53:32 -0400

    added sender and receivers to administration

--- apps/compras/compras/signals.py
+++ apps/compras/compras/signals.py
@@ -1,8 +1,10 @@
-# from apps.almacen.signals import productosSignal
 from django.dispatch import Signal
 
+# from apps.almacen.signals import productosSignal
+# from apps.compras.otros_signals import aceptado_signal
+
 # Función receptora de la señal productosSignal
-# @productosSignal.connect
+
 def productos_bajos_receiver(sender, **kwargs):
     productos = kwargs.get('productos')
     if productos:
@@ -15,11 +17,24 @@ def productos_bajos_receiver(sender, **kwargs):
                 cantidad=getattr(producto, 'cantidad', 0),
                 medida=getattr(producto, 'medida', '')
             )
+# productosSignal.connect(productos_bajos_receiver)
+
+
+#  Función receptora de la señal aceptado_signal
+def aceptado_receiver(sender, **kwargs):
+    aceptado = kwargs.get('aceptado')
+    mensaje = kwargs.get('mensaje')
+    print(f"Receiver: aceptado={aceptado}, mensaje={mensaje}")
+# aceptado_signal.connect(aceptado_receiver)
+
+#  Senders
 
 monto_mensaje_signal = Signal()
 
-def enviar_monto_mensaje(monto, mensaje):
+def enviar_monto_mensaje(monto, mensaje=None):
     """
     Envía un signal con un monto y un mensaje usando Django signals.
     """
+    if mensaje is None:
+        mensaje = "Solicitud de Compra"
     monto_mensaje_signal.send(sender=None, monto=monto, mensaje=mensaje)
